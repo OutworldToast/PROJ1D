8e9f3b89ffb54367d32817d14fef613b76fbdd1b
Batu-C <22057870@student.hhs.nl>
2023-03-16 09:44:00 +0100

    Opties

--- src/main/java/org/example/Lijst.java
+++ src/main/java/org/example/Lijst.java
@@ -1,20 +1,44 @@
 package org.example;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Lijst {
 
     ArrayList<Offerteoptie> optielijst = new ArrayList<>();
+    ArrayList<Offerteoptie> gekozenlijst = new ArrayList<>();
 
     Lijst(){
-        File file = new File("Opties.txt");
-        Scanner scanner = new Scanner(System.in);
+        String line = "";
+        String splitBy = ",";
+        try {
+
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource("opties.csv").getFile());
+            InputStream is = new FileInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            while ((line = br.readLine()) != null) {
+                String[] opties = line.split(splitBy);
+                Offerteoptie optie = new Offerteoptie();
+                optie.naam = opties[0];
+                optie.categorie = opties[1];
+                optie.prijs = Double.parseDouble(opties[2]);
+                optielijst.add (optie);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void PrintLijst(){
+        for (Offerteoptie optie : optielijst) {
+            System.out.printf("Naam: %s, Categorie: %s, Prijs: €%.2f%n", optie.naam, optie.categorie, optie.prijs);
+        }
     }
     static class Offerteoptie {
-        String optie;
-        String type;
+        String naam;
+        String categorie;
         double prijs;
     }
 

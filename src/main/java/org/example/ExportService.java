898ba2b1fd2145b2b047cdde2af3e1da5966e4c4
Richano <rishanogopal@gmail.com>
2023-03-16 10:10:26 +0100

    Add export class

---
+++ src/main/java/org/example/ExportService.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class ExportService {
+    private ArrayList<String> offerteOpties = new ArrayList<>();
+
+    public static void exportOfferte() {
+        System.out.println("---- Offerte: TITLE\n");
+
+        System.out.println("Klanttype: blablabla");
+
+        System.out.println("Optie 1: blablabla");
+        System.out.println("Optie 2: blablabla");
+
+        System.out.println("Totaalprijs: $0");
+    }
+
+}

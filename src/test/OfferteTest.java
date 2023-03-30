ca30d9aa7b348c2afdd5e58510ef10ecafc9c480
Richano <rishanogopal@gmail.com>
2023-03-30 09:14:04 +0200

    Fixes

--- src/test/OfferteTest.java
+++ src/test/OfferteTest.java
@@ -1,4 +1,5 @@
 import org.example.OptieLijst;
+import org.example.TotaalLijst;
 import org.example.klant.Klant;
 import org.example.klant.Particulier;
 import org.example.offerte.Offerte;
@@ -11,7 +12,7 @@ public class OfferteTest {
     @Test
     public void printTotaalPrijsTest() {
         Klant klant = new Particulier("Mark", "markie@hotmail.com");
-        OptieLijst opties = new OptieLijst();
+        TotaalLijst opties = new TotaalLijst();
         opties.addOptie("Test Optie 1", "Essentieel", 25.0, 0);
         opties.addOptie("Test Optie 2", "Essentieel", 21.50, 0);
         Offerte offerte = new Offerte("Test beschrijving", klant, opties);

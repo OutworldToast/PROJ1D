41d613c5455fb8b85bf0700fb7874ad687cd9e7a
Richano <rishanogopal@gmail.com>
2023-04-03 14:59:58 +0200

    Fixes

--- src/main/java/org/example/PrijsOpgave.java
+++ src/main/java/org/example/PrijsOpgave.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.klant.Klant;
+import org.example.offerte.Offerte;
 
 import java.sql.SQLOutput;
 import java.util.Date;
@@ -14,7 +15,6 @@ public class PrijsOpgave {
     private int arbeidsuren;
     private double uurtarief;
 
-<<<<<<< HEAD
     private Offerte offerte;
 
     private TotaalLijst totaalLijst;
@@ -26,18 +26,12 @@ public class PrijsOpgave {
 
 
 
-    public PrijsOpgave(Klant klantnaam, Offerte offerte, TotaalLijst totaalLijst) {
-        this.klantnaam = klantnaam;
-        this.offerte = offerte;
-        this.totaalLijst = totaalLijst;
-=======
     public PrijsOpgave(Klant klant, String specificaties, double materialeKosten, int arbeidsuren, double uurtarief) {
         this.klant = klant;
         this.specificaties = specificaties;
         this.materialeKosten = materialeKosten;
         this.arbeidsuren = arbeidsuren;
         this.uurtarief = uurtarief;
->>>>>>> parent of f4e9fce (prisjopgave)
     }
 
     public Klant getKlant() {
@@ -80,70 +74,9 @@ public class PrijsOpgave {
         this.uurtarief = uurtarief;
     }
 
-<<<<<<< HEAD
-    public void toonPrijsopgave() {
-
-        String strOfferte = "PRIJSOPGAVE";
-        String strResult = "TOTAAL ONDERDELEN: ";
-        String strHoeveelheid = "HVH";
-        String strBeschrijving  = "BESCHRIJVING";
-        String strVerkoper = "VERKOPER";
-        String strPrijsPerEenheid = "PRIJS PER EENHEID";
-        String strRegelTotaal = "REGELTOTAAL";
-        String strKorting = "KORTING";
-        String strBtw = "BTW";
-        String strCategorie = "CATEGORIE";
-
-        double totaal = 0;
-        double result = 0;
-        int teller = 1;
-
-        System.out.printf( "%60s\n", strOfferte);
-        System.out.print("-------------------------------------------------------------------------------------------------------");
-        System.out.println();
-
-        System.out.println("02-01-2023");
-        System.out.println("AAN: "+ klantnaam.getNaam());
-        System.out.printf("%-20s | %-20s| %-20s | %-20s  | %-20s\n ", strHoeveelheid,strCategorie, strBeschrijving,strPrijsPerEenheid, strRegelTotaal);
-        System.out.print("-------------------------------------------------------------------------------------------------------\n");
-
-
-
-
-            for (Onderdeel t : totaalLijst.getTotaalLijst()
-            ) {
-                String strgezochtOnderdeel = t.getNaam();
-                 result += totaal + t.getPrijs();
-                 if (t.equals(strgezochtOnderdeel)){
-                     teller++;
-                 }
-
-
-
-                System.out.printf("%-20s   %-20s  %-20s  € %.2f%n", teller, t.getCategorie(), t.getNaam(), t.getPrijs()); // HOVEELHEID PER ONDERDEEL
-                System.out.println();
-
-            }
-        System.out.print("-------------------------------------------------------------------------------------------------------\n");
-
-        System.out.printf("%-60s\n", strKorting); //
-        System.out.printf("%-60s\n", strBtw ); // BEREKEN BTW
-        System.out.printf("%-90s  € %.2f%n", strResult,  result );
-
-
-
-
-
-
-
-
-
-
-=======
     public double berekenTotaleKosten() {
         return getMaterialeKosten()  + (getArbeidsuren() * getUurtarief());
     }
->>>>>>> parent of f4e9fce (prisjopgave)
 
     public void printPrijsopgave() {
         System.out.println("Prijsopgave voor " + klant.getNaam());

c5a2c8973c2797520f0264051155d33babb6a6b4
Richano <rishanogopal@gmail.com>
2023-04-13 10:40:36 +0200

    Add test klant

---
+++ src/test/java/org/example/KlantTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.klant.Bedrijf;
+import org.example.klant.Klant;
+import org.example.schip.Schip;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.testng.annotations.Ignore;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KlantTest {
+    @Test
+    void klantKortingPercentage(){
+        Klant klant = new Bedrijf("Bedrijfje", "123@hotmail.nl", 6969);
+
+        klant.setKortingAlsPercentage(20);
+
+        assertEquals(klant.getKortingAlsPercentage(), 20);
+    }
+}
